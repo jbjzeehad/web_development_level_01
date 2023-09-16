faccbab495ebafb4f0c820b05b0172a844b6c045
jbjzeehad <ilamta.97@gmail.com>
2023-09-16 16:09:27 +0600

    js variable

--- milestone_04/module_16/first.js
+++ milestone_04/module_16/first.js
@@ -1,4 +1,5 @@
 
 console.log(5);
 console.log(4);
-console.log("hi i am Jubayer")
+console.log("hi i am Jubayer");
+console.log("ki korbo jibone");
---
+++ milestone_04/module_16/math.js
@@ -0,0 +1,6 @@
+
+
+var price = 13;
+var age = 17;
+var temperature = 38;
+console.log(age);
