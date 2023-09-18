47c148bcca0549a7ff9134017976df6ccc278014
jbjzeehad <ilamta.97@gmail.com>
2023-09-18 15:04:46 +0600

    loop

---
+++ milestone_04/module_18/revision.js
@@ -0,0 +1,12 @@
+var bottle = "Water";
+var bottleColor = 'yeollw';
+var sunglassPrice = 120;
+var penaColor = 'blue';
+var isExpensive = true;
+var tableItems = ['blue', 'red', 'green'];
+var item3 = tableItems[3];
+tableItems[2] = 'notevook';
+var penIndex = tableItems.indexOf('red');
+if (tableItems.length > 4) {
+
+}
