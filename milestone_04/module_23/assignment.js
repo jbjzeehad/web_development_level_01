0e135e14e078a5e7a1a9427051bc4d0c796f9fed
jbjzeehad <ilamta.97@gmail.com>
2023-09-24 02:50:30 +0600

    assignment done

--- milestone_04/module_23/assignment.js
+++ milestone_04/module_23/assignment.js
@@ -38,15 +38,15 @@ function sortMaker(arr) {
 
 function findAddress(obj) {
     let streetadd = obj.street;
-    if (typeof streetadd == 'undefined') {
+    if (typeof streetadd === 'undefined') {
         streetadd = "__";
     }
     let houseadd = obj.house;
-    if (typeof houseadd == 'undefined') {
+    if (typeof houseadd === 'undefined') {
         houseadd = "__";
     }
     let societyadd = obj.society;
-    if (typeof societyadd == 'undefined') {
+    if (typeof societyadd === 'undefined') {
         societyadd = "__";
     }
     let finaladd = streetadd + "," + houseadd + "," + societyadd;
@@ -71,8 +71,8 @@ function canPay(changeArray, totalDue) {
 
 // check function
 // -----------------------------------
-// console.log(cubeNumber(3));
-// console.log(cubeNumber(4));
+console.log(cubeNumber(3));
+console.log(cubeNumber(4));
 // console.log(cubeNumber(-3));
 // console.log(cubeNumber(0));
 // console.log(cubeNumber('3'));
@@ -83,53 +83,57 @@ function canPay(changeArray, totalDue) {
 // console.log(matchFinder(2, 'bijoy'));
 // console.log(matchFinder(2, 3));
 // console.log(matchFinder('bijoy', 'joy'));
-// console.log(matchFinder('John Doe', 'ohn'));
-// console.log(matchFinder('JavaScript', 'Scr'));
-// console.log(matchFinder('Peter Parker', 'ker'));
-// console.log(matchFinder('Peter Parker', ' Par'));
+console.log(matchFinder('John Doe', 'ohn'));
+console.log(matchFinder('JavaScript', 'Code'));
+console.log(matchFinder('Peter Parker', 'Pen'));
+console.log(matchFinder('Peter Parker', 'pet'));
 // -------------------------------------------------
 // let ara1 = [0, 1];
 // let ara2 = [-4, 4];
 // let ara3 = [4, -4];
 // let ara4 = [4, 3];
 // let ara5 = [4, -5];
-// let ara6 = [2, 3];
-// let ara7 = [4, 2];
-// let ara8 = [4, 4];
-// let ara9 = [1, 2];
-// let ara10 = [4, -2];
+let ara6 = [2, 3];
+let ara7 = [4, 2];
+let ara8 = [4, 4];
+let ara9 = [1, 2];
+let ara10 = [4, -2];
 // let ara11 = [1, 0];
 // console.log(sortMaker(ara1));
 // console.log(sortMaker(ara2));
 // console.log(sortMaker(ara3));
 // console.log(sortMaker(ara4));
 // console.log(sortMaker(ara5));
-// console.log(sortMaker(ara6));
-// console.log(sortMaker(ara7));
-// console.log(sortMaker(ara8));
-// console.log(sortMaker(ara9));
-// console.log(sortMaker(ara10));
+console.log(sortMaker(ara6));
+console.log(sortMaker(ara7));
+console.log(sortMaker(ara8));
+console.log(sortMaker(ara9));
+console.log(sortMaker(ara10));
 // console.log(sortMaker(ara11));
 // ----------------------------------------------
 
-// let add = { street: 10, house: "15A", society: "Earth Perfect" };
-// let add1 = { house: "15A", society: "Earth Perfect" };
-// let add2 = { street: 11, society: "Only Perfect" };
-// let add3 = { street: 12 };
-// let add4 = {};
-// console.log(findAddress(add));
-// console.log(findAddress(add1));
-// console.log(findAddress(add2));
+let add = { street: 10, house: "15A", society: "Earth Perfect" };
+let add1 = { street: 10, society: "Earth Perfect" };
+let add2 = { street: 10 };
+// let add3 = { house: "15A", society: "Earth Perfect" };
+// let add4 = { street: 11, society: "Only Perfect" };
+// let add5 = { street: 12 };
+// let add6 = {};
+console.log(findAddress(add));
+console.log(findAddress(add1));
+console.log(findAddress(add2));
 // console.log(findAddress(add3));
 // console.log(findAddress(add4));
+// console.log(findAddress(add5));
+// console.log(findAddress(add6));
 // ------------------------------------------
 
 // let money = [2, 4, 5];
-// let money1 = [1, 2, 5];
-// let money2 = [1, 5, 5];
+let money1 = [1, 2, 5];
+let money2 = [1, 5, 5];
 // let money3 = [];
-// let price = 10;
+let price = 10;
 // console.log(canPay(money, price));
-// console.log(canPay(money1, price));
-// console.log(canPay(money2, price));
+console.log(canPay(money1, price));
+console.log(canPay(money2, price));
 // console.log(canPay(money3, price));
