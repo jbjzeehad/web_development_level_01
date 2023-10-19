664a41a7191ba98dd2d0b44a512f7547f245cd60
jbjzeehad <ilamta.97@gmail.com>
2023-10-19 20:31:57 +0600

    array and object destructing

---
+++ milestone_07/module_37/array-methods.js
@@ -0,0 +1,17 @@
+const products = [
+    { name: 'laptop', price: 2300, brand: 'lenevo', color: 'silver' },
+    { name: 'phone', price: 2300, brand: 'iphone', color: 'golden' },
+    { name: 'watch', price: 300000, brand: 'casio', color: 'black' },
+    { name: 'sunglass', price: 12300, brand: 'canon', color: 'blue' },
+];
+
+const brands = products.map(product => product.brand);
+// console.log(brands);
+products.forEach(product => console.log(product));
+products.forEach(product => console.log(product.color));
+const cheap = products.filter(product => product.price <= 10000);
+const specificname = products.filter(pro => pro.name.includes('n'));
+const special = products.find(prod => prod.name.includes('n'));
+console.log(cheap);
+console.log(specificname);
+console.log(special);
