f78e6f14a856e2d66dbd516ab4965bdaececdf7f
jbjzeehad <ilamta.97@gmail.com>
2023-10-20 21:07:58 +0600

    React Journey

---
+++ explore-react-core-concepts/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Person></Person>
+      <Student></Student>
+    </>
+  )
+}
+function Person() {
+  const age = 23;
+  const money = 20;
+  const person = { name: 'Sakib', age: 12 }
+  return <h3>I am {person.name} with age: {age + money}</h3>
+}
+function Student() {
+  return (<div>
+    <h3>This is a student</h3>
+    <p>Name:</p>
+    <p>Age:</p>
+  </div>
+  )
+}
+
+export default App
