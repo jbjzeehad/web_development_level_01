fc66de23f9d3fba441163604b540fb141ff4f13b
jbjzeehad <ilamta.97@gmail.com>
2023-10-24 23:37:42 +0600

    react base

--- react-world-tour/src/components/Country/Country.jsx
+++ react-world-tour/src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import './Country.css'
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry }) => {
     const { name, flags, population, area, cca3 } = country;
 
     const [Visited, setVisited] = useState(false);
@@ -10,13 +10,19 @@ const Country = ({ country }) => {
         setVisited(!Visited);
     }
 
+    const passWithParams = () => {
+        handleVisitedCountry(country);
+    }
+
+
     return (
-        <div className="country">
-            <h3>{name.common}</h3>
+        <div className={`country ${Visited ? 'visited' : 'non-visited'}`}>
+            <h3 style={{ color: Visited ? 'white' : 'red' }}>{name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population: {population}</p>
             <p>area: {area}</p>
             <p><small>Code: {cca3}</small></p>
+            <button onClick={passWithParams}>Mark Visited</button>
             <button onClick={handleVisited}>{Visited ? 'Visited' : 'Going'}</button>
             {Visited ? 'I have visited this country' : 'I want to visit'}
 
