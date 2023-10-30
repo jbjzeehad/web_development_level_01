c75c22d554b9d371d315003ff51909b92ba77081
jbjzeehad <ilamta.97@gmail.com>
2023-10-30 23:37:16 +0600

    spa project

--- react-world-tour/src/components/Countries/Countries.jsx
+++ react-world-tour/src/components/Countries/Countries.jsx
@@ -5,6 +5,7 @@ import './Countries.css'
 const Countries = () => {
     const [countries, setCountries] = useState([]);
     const [visitedCountries, setVisitedCountries] = useState([]);
+    const [visitedFlags, setVisitedFlags] = useState([])
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
