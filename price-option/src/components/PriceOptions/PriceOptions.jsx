fa716d222f7d1940946047cc6c7ab00201e3a335
jbjzeehad <ilamta.97@gmail.com>
2023-11-03 23:59:15 +0600

    react

---
+++ price-option/src/components/PriceOptions/PriceOptions.jsx
@@ -0,0 +1,70 @@
+import PriceOption from "../PriceOption/PriceOption";
+
+
+const PriceOptions = () => {
+
+    const priceOptions = [
+        {
+            "id": 1,
+            "name": "Basic Membership",
+            "price": "$29.99 per month",
+            "features": [
+                "Access to gym facilities",
+                "Use of cardio equipment",
+                "Locker room access",
+                "1 complimentary fitness assessment",
+                "Access to the swimming pool",
+                "Access to the sauna",
+                "Discounts on gym merchandise"
+            ]
+        },
+        {
+            "id": 2,
+            "name": "Premium Membership",
+            "price": "$49.99 per month",
+            "features": [
+                "Access to gym facilities",
+                "Use of cardio and strength training equipment",
+                "Group fitness classes",
+                "Locker room access",
+                "2 complimentary fitness assessments",
+                "Access to the swimming pool",
+                "Access to the sauna",
+                "Discounts on gym merchandise",
+                "Unlimited towel service"
+            ]
+        },
+        {
+            "id": 3,
+            "name": "VIP Membership",
+            "price": "$99.99 per month",
+            "features": [
+                "Unlimited access to gym facilities",
+                "Use of cardio and strength training equipment",
+                "Group fitness classes",
+                "Locker room access with sauna and steam room",
+                "4 complimentary fitness assessments",
+                "Access to the swimming pool",
+                "Access to the sauna",
+                "Discounts on gym merchandise",
+                "Unlimited towel service",
+                "Personal training session (once a month)",
+                "Nutrition consultation (quarterly)"
+            ]
+        }
+    ];
+
+    return (
+        <div>
+            <div>
+                <h2 className="text-5xl">Best Prices in the town</h2>
+                {
+                    priceOptions.map(option => <PriceOption key={option.id} option={option}></PriceOption>)
+                }
+            </div>
+
+        </div>
+    );
+};
+
+export default PriceOptions;
