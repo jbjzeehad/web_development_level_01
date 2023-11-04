42cd4b50c7dff6ca917630c2958ba0c6580916e2
jbjzeehad <ilamta.97@gmail.com>
2023-11-04 21:48:06 +0600

    react-routing

---
+++ inroduction-to-react-router/src/main.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home/Home.jsx';
+import About from './Components/About/About.jsx';
+import Contact from './Components/Contact/Contact.jsx';
+import Users from './Components/Users/Users.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/about',
+        element: <About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+
+      },
+      {
+        path: '/users',
+        element: <Users></Users>
+
+      }
+    ]
+  },
+
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+
+  </React.StrictMode>,
+)
