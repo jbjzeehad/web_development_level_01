09a5ee4139995b65b76dccffedbfcf5a2b50ce08
jbjzeehad <ilamta.97@gmail.com>
2023-11-05 13:07:11 +0600

    react routing, dynamic

--- inroduction-to-react-router/src/main.jsx
+++ inroduction-to-react-router/src/main.jsx
@@ -10,6 +10,9 @@ import Home from './Components/Home/Home.jsx';
 import About from './Components/About/About.jsx';
 import Contact from './Components/Contact/Contact.jsx';
 import Users from './Components/Users/Users.jsx';
+import UserDetails from './Components/UserDetails/UserDetails.jsx';
+import Posts from './Components/Posts/Posts.jsx';
+import PostDetails from './Components/PostDetails/PostDetails.jsx';
 
 const router = createBrowserRouter([
   {
@@ -29,8 +32,23 @@ const router = createBrowserRouter([
         path: '/users',
         loader: () => fetch('https://jsonplaceholder.typicode.com/users'),
         element: <Users></Users>
-
+      },
+      {
+        path: '/user/:userId',
+        loader: ({ params }) => fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
+        element: <UserDetails></UserDetails>
+      },
+      {
+        path: '/posts',
+        loader: ({ params }) => fetch(`https://jsonplaceholder.typicode.com/posts`),
+        element: <Posts></Posts>
+      },
+      {
+        path: '/post/:postId',
+        loader: ({ params }) => fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`),
+        element: <PostDetails></PostDetails>
       }
+
     ]
   },
 
