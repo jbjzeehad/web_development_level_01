3f404eb1a5ebbb64f03f856f827f98120d1e0d28
jbjzeehad <ilamta.97@gmail.com>
2023-11-05 15:44:38 +0600

    reaact router

--- inroduction-to-react-router/src/Components/Header/Header.jsx
+++ inroduction-to-react-router/src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
@@ -7,9 +7,9 @@ const Header = () => {
             <span>My website</span>
             <nav>
                 <Link to='/'>Home</Link>
-                <Link to='/users'>Users</Link>
-                <Link to='/posts'>Posts</Link>
-                <Link to='/about'>About</Link>
+                <NavLink to='/users'>Users</NavLink>
+                <NavLink to='/posts'>Posts</NavLink>
+                <NavLink to='/about'>About</NavLink>
                 <Link to='/contact'>Contact Us</Link>
 
             </nav>
--- inroduction-to-react-router/src/Components/Home/Home.jsx
+++ inroduction-to-react-router/src/Components/Home/Home.jsx
@@ -1,12 +1,16 @@
 
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 const Home = () => {
+    const navigation = useNavigation();
     return (
         <div>
             <Header></Header>
+            {
+                navigation.state === 'loading' ? <p>Loading...</p> : <Outlet></Outlet>
+            }
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
