8929ec40849e48a3ee891855d2db7a5baaecb8ec
jbjzeehad <ilamta.97@gmail.com>
2023-11-08 17:14:14 +0600

    react project

---
+++ react-career-hub/src/Components/CategoryList/CategoryList.jsx
@@ -0,0 +1,12 @@
+
+
+const CategoryList = () => {
+    return (
+        <div>
+            <h2 className="text-5xl text-center">Job category List</h2>
+            <p className="text-center">Explore thousand of job opportunitiew with alll the information you need . Its your future</p>
+        </div>
+    );
+};
+
+export default CategoryList;
